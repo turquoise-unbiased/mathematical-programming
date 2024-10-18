c3bc5229181eaf10c7a3dcbc933c167506014e53
Wojciech Lawren <wojciech.lawren@gmail.com>
2024-10-19 01:07:15 +0200

    parallel compute array

--- opt_uninte.R
+++ opt_uninte.R
@@ -3,10 +3,7 @@
 library(parallel)
 
 ## mode : numeric | typeof : double | str : array
-a.1 <- 1e2 ; b.1 <- 1e5 ; l.1 <- 1e2  # random generator parameters
-a.2 <- 1e3 ; b.2 <- 1e5 ; l.2 <- 1e3  # random generator parameters
-k   <- 2  # rng sets
-n   <- 3  # trials n ^ 3 * k
+n <- 3  # trials n ^ 3 * n
 
 S.show <- T  # print score on terminal
 S.save <- F  # save score to file
@@ -16,19 +13,16 @@ ff <- cumprod(seq(n))  # cumulative product vector 1:n series coefficient
 
 p.switch = function(i) {
   switch(i[1],
-    rep(ff * l.1, times = n ^ 2),                     # n ^ 3
-    rep(ff * a.1, each  = n, times = n),
-    rep(ff * b.1, each  = n ^ 2),
-    rep(ff * l.2, times = n ^ 2),                     # n ^ 3
-    rep(ff * a.2, each  = n, times = n),
-    rep(ff * b.2, each  = n ^ 2))
+    rep(ff * sample(ff * 5e2, 1), times = n ^ 2),     # n ^ 3
+    rep(ff * sample(ff * 1e3, 1), each  = n, times = n),
+    rep(ff * sample(ff * 1e5, 1), each  = n ^ 2))
 }
 
 parameters = function() {
-  cl = makeForkCluster(min(k * 3, cc))                # cluster of processes
+  cl = makeForkCluster(min(n * 3, cc))                # cluster of processes
   on.exit(stopCluster(cl))
 
-  P <- array(seq(k * 3), dim = c(1, 3, k))            # parameters array
+  P <- array(rep(seq(3), times = n), dim = c(1, 3, n)) # parameters array
   parApply(cl, P, c(2, 3), function(i) p.switch(i))   # parallel apply
 }
 
@@ -50,7 +44,7 @@ orbit = function(r) {
 S.col <- factor(c("intvl", "n.rand", "dist.men", "dist.med", "dist.mad", "coll.rand", "coll.dist"))
 
 simulate = function(P) {
-  cl = makeForkCluster(min(n ^ 3 * k, cc))            # cluster of processes
+  cl = makeForkCluster(min(n ^ 3 * n, cc))            # cluster of processes
   on.exit(stopCluster(cl))
 
   parApply(cl, P, c(1, 3), function(r) orbit(r))      # parallel apply
@@ -58,5 +52,5 @@ simulate = function(P) {
 
 S <- aperm(simulate(parameters()), perm = c(2, 1, 3)) ; colnames(S) <- S.col  # score array
 
-if (S.show) { for (i in seq(k)) { print(summary(S[,,i])) ; print(cor(S[,,i])) }}
+if (S.show) { for (i in seq(n)) { print(summary(S[,,i])) ; print(cor(S[,,i])) }}
 if (S.save) { write.csv(S, "data.csv") }
