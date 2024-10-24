0316291c48fa2d245b84946940c996d4d20fb266
Wojciech Lawren <wojciech.lawren@gmail.com>
2024-10-24 10:15:49 +0200

    prototype of object oriented parallel sim

--- opt_uninte.R
+++ opt_uninte.R
@@ -1,24 +1,27 @@
 ## opt_uninte.R [4.4.1] 2024, Wojciech Lawren, All rights reserved.
 ## Optimizing [p.r.] moment / interval | collision of pseudo-random integers of uniform distribution
-library(parallel)
-cc <- detectCores()  # number of CPU cores >=1 nodes involved in simulation
+library("parallel")
+.cc <- parallel::detectCores()  # number of CPU cores >=1 nodes involved in simulation
 
 source("cls_uninte.R")
-m <- new("Sim")  # object class Sim
+assign("m", new("Sim"), envir = ( e.sim <- new.env() ))  # object class Sim
 
-parameters = function() {
-  cl = makeForkCluster(min(3L * m@k, cc))                       # cluster of processes
-  on.exit(stopCluster(cl))
-  parApply(cl, m@P, c(2L, 3L), function(i) p.switch(i, m))      # parallel apply
+parameters <- function() {
+  cl <- parallel::makeForkCluster(min((3L * m@k), .cc))                       # cluster of processes
+  on.exit(parallel::stopCluster(cl))
+  # parallel::clusterEvalQ(cl, attach(e.sim, pos = 2L))
+  parallel::parApply(cl, eval(m@A), c(2L, 3L), function(r) v.switch(r))      # parallel apply
 }
 
-simulate = function(P) {
-  cl = makeForkCluster(min(m@t ^ 3L * m@k, cc))                 # cluster of processes
-  on.exit(stopCluster(cl))
-  parApply(cl, P, c(1L, 3L), function(r) orbit(r))              # parallel apply
+simulate <- function(P) {
+  cl <- parallel::makeForkCluster(min((m@t ^ 3L * m@k), .cc))                 # cluster of processes
+  on.exit(parallel::stopCluster(cl))
+  parallel::parApply(cl, P, c(1L, 3L), function(r) orbit(r))              # parallel apply
 }
 
-S <- aperm(simulate(parameters()), perm = c(2L, 1L, 3L)) ; colnames(S) <- m@col  # score array
+attach(e.sim, pos = 2L)
+S <- aperm(simulate(parameters()), perm = c(2L, 1L, 3L))  # score array
 
+colnames(S) <- m@col
 if (m@show) { for (i in seq(m@k)) { print(summary(S[,,i])) ; print(cor(S[,,i])) }}
 if (m@save) { write.csv(S, m@csv) }
