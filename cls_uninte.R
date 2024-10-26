0068e2ee25f36cd88428dd4cb5e8d1deabafb965
Wojciech Lawren <wojciech.lawren@gmail.com>
2024-10-26 15:22:32 +0200

    prototype of functional parallel sim

--- cls_uninte.R
+++ cls_uninte.R
@@ -3,10 +3,9 @@
 
 ## Classes ##
 
-UN  <- setClass("UN", contains = c("VIRTUAL"))
+setClass("UN",  contains  = c("VIRTUAL"), sealed = T)
 
-Log <- setClass("Log",
-                contains  = c("UN"),
+setClass("Log", contains  = c("UN"), sealed = T,
                 slots     = list(show = "logical",         # print score on terminal
                                  save = "logical",         # save score to file
                                  csv  = "character"),      # csv file to save
@@ -15,68 +14,61 @@ Log <- setClass("Log",
                                  csv  = "data.csv"),
                 validity  = function(object) {})
 
-Col <- setClass("Col",
-                contains  = c("UN"),
+setClass("Col", contains  = c("UN"), sealed = T,
                 slots     = list(col = "factor"),          # score array colnames
                 prototype = list(col = factor(c("intvl", "n.rand", "dist.men", "dist.med", "dist.mad",
                                                 "coll.rand", "coll.dist"))),
                 validity  = function(object) {})
 
-Lab <- setClass("Lab",
-                contains  = c("Log", "Col"),
+setClass("Lab", contains  = c("Log", "Col"), sealed = T,
                 slots     = list(n = "numeric",            # runif n
                                  a = "numeric",            # runif min
                                  b = "numeric",            # runif max
                                  t = "numeric",            # trials t ^ 3 * k
                                  k = "numeric"),           # score array dim 3
-                prototype = list(n = 1e2,
-                                 a = 1e2,
-                                 b = 1e5,
+                prototype = list(n = 1e+03,
+                                 a = 1e+03,
+                                 b = 1e+06,
                                  t = 3L,
                                  k = 2L),
                 validity  = function(object) {})
 
-Sim <- setClass("Sim",
-                contains  = c("Lab"),
-                slots     = list(ff = "vector",            # cumulative product vector 1:t series coefficients
-                                 M  = "matrix",            # ff * n|a|b
-                                 P  = "array"),            # parameters array
+setClass("Sim", contains  = c("Lab"), sealed = T,
+                slots     = list(A  = "call",            # parameters array call
+                                 ff = "numeric"),            # cumulative product vector 1:t series coefficients
                 validity  = function(object) {})
 
 ## Methods ##
 
-setMethod("initialize", signature = c("Sim"),                                  # initialize Sim slots
+setMethod("initialize", sealed = T, signature = c("Sim"),                                  # initialize Sim slots
           definition = function(.Object, ...) {
             .Object    <- callNextMethod(.Object, ...)
+            .Object@A  <- substitute(array(data = rep(seq(3L), times = k),
+                                           dim  = c(1L, 3L, k)), list(k = .Object@k))
             .Object@ff <- cumprod(seq(.Object@t))
-            .Object@M  <- matrix(c(.Object@ff * .Object@n, .Object@ff * .Object@a,
-              .Object@ff * .Object@b), ncol = 3L)
-            .Object@P  <- array(rep(seq(3L), times = .Object@k), dim = c(1L, 3L, .Object@k))
             .Object
           })
 
-setGeneric("p.switch", def = function(i, m) standardGeneric("p.switch"))       # switch parameters sets
-setMethod("p.switch", signature = c("numeric", "Sim"),
-          definition = function(i, m) {
-            switch(i[1L],
-              rep(m@ff * sample(m@M[,1L], 1L), times = m@t ^ 2L),              # t ^ 3
-              rep(m@ff * sample(m@M[,2L], 1L), each  = m@t, times = m@t),
-              rep(m@ff * sample(m@M[,3L], 1L), each  = m@t ^ 2L))
-          })
+## Functions ##
 
-setGeneric("orbit", def = function(r) standardGeneric("orbit"))                # compute runif stat trial
-setMethod("orbit", signature = c("numeric"),
-          definition = function(r) {
-            r.n <- r[1L] ; r.a <- r[2L] ; r.b <- r[3L]
+v.switch <- as.function(alist(r =, {                                           # switch parameters sets
+  switch(r,
+    rep((m@ff * rbinom(1L, m@n, runif(1L, 2e-01))), times = m@t ^ 2L),              # t ^ 3
+    rep((m@ff * rbinom(1L, m@a, runif(1L, 2e-01))), each  = m@t, times = m@t),
+    rep((m@ff * rbinom(1L, m@b, runif(1L, 2e-01))), each  = m@t ^ 2L))
+}))
 
-            ran.v <- round(runif(r.n, r.a, r.b))                               # random integers vector
-            v.dif <- abs(diff(ran.v, lag = 1L))                                # neighbors distance vector |i,j|
+orbit <- as.function(alist(r =, {                                              # compute runif stat trial
+  r.n <- r[1L] ; r.a <- r[2L] ; r.b <- r[3L]
 
-            d.men <- mean(v.dif)                                               # mean distance
-            d.med <- median(v.dif)                                             # median distance
-            d.mad <- mad(v.dif, center = d.med, constant = 1L)                 # median absolute deviation of distance
-            ran.c <- length(ran.v) - length(unique(ran.v))                     # integers collision
-            dif.c <- length(v.dif) - length(unique(v.dif))                     # distance collision
+  ran.v <- floor(runif(r.n, min = r.a, max = r.b))                              # random integers vector
+  v.dif <- abs(diff(ran.v, lag = 1L, differences = 1L))                         # neighbors distance vector
 
-            round(c(r.b - r.a, r.n, d.men, d.med, d.mad, ran.c, dif.c))
-          })
+  d.men <- mean(v.dif)                                               # mean distance
+  d.med <- median(v.dif)                                             # median distance
+  d.mad <- mad(v.dif, center = d.med, constant = 1L)                # median absolute deviation of distance
+  ran.c <- sum(duplicated(ran.v))                     # integers collision
+  dif.c <- sum(duplicated(v.dif))                     # distance collision
+
+  round(c((r.b - r.a), r.n, d.men, d.med, d.mad, ran.c, dif.c), 3L)
+}))
