0a9f6fcda7e553dd49cb26b85b5f8a4f0a5d496d
Wojciech Lawren <wojciech.lawren@gmail.com>
2024-12-09 14:21:10 +0100

    seq::par numeric conform

--- bench_parallel.cpp
+++ bench_parallel.cpp
@@ -14,7 +14,7 @@
 #include <vector>
 
 typedef double F;  // float, double, long double
-constexpr auto N = 100000L;
+constexpr auto N = 1'000'000L;
 using namespace oneapi::tbb;
 
 void fn() {
@@ -23,22 +23,20 @@ void fn() {
   static concurrent_queue<tick_count> t;
   constexpr auto f1 = [&]() { { t.try_pop(t0); t.try_pop(t1); } return ((t1 - t0).count() / 1e+03); };
   // random number generator
-  speculative_spin_mutex m;
   std::default_random_engine generator(37u);
   std::uniform_real_distribution<F> distribution(1e0, (N / 2e0));
-  std::function<F()> roller = [&]() {
-    speculative_spin_mutex::scoped_lock lock_shared(m); return distribution(generator); };
+  std::function<F()> roller = [&]() { return distribution(generator); };
   // container vector
   std::vector<F> vd((N | 1L));
   const auto v = std::span<F>(vd);
   // parallel generate roller
   t.push(tick_count::now());
-  std::generate(std::execution::par, v.begin(), v.end(), roller);
+  std::generate(std::execution::seq, v.begin(), v.end(), roller);
   t.push(tick_count::now());
 
   // sum, mean, median, mad
   t.push(tick_count::now());
-  const auto v_sum = std::reduce(std::execution::par, v.begin(), v.end(), F(0L), std::plus<F>());
+  const auto v_sum = std::accumulate(v.begin(), v.end(), F(0L));
   t.push(tick_count::now());
 
   const auto v_mean = v_sum / v.size();
@@ -51,7 +49,7 @@ void fn() {
 
   t.push(tick_count::now());
   std::transform(std::execution::par, v.begin(), v.end(), v.begin(),
-                 [&](const auto &elem) { return abs(elem - v_median); });
+                 [&](const auto &elem) { return fabs(elem - v_median); });
   t.push(tick_count::now());
 
   t.push(tick_count::now());
@@ -62,15 +60,15 @@ void fn() {
 
   // print stats
   printf( "A) trial size (ff)     [el]:          %zu\n",  v.size()                       );
-  printf( "1) parallel generate   [us]:          %.3f\n", f1()                           );
-  printf( "2) parallel reduce     [us]:          %.3f\n", f1()                           );
+  printf( "1) generate            [us]:          %.3f\n", f1()                           );
+  printf( "2) accumulate          [us]:          %.3f\n", f1()                           );
   printf( "3) parallel stable_sort[us]:          %.3f\n", f1()                           );
   printf( "4) parallel transform  [us]:          %.3f\n", f1()                           );
   printf( "5) parallel stable_sort[us]:          %.3f\n", f1()                           );
-  printf( "1) sum: sum(v)                        %.3f\n", v_sum                          );
-  printf( "2) mean: sum/size(v)                  %.3f\n", v_mean                         );
-  printf( "3) median: sort(v)[size(v)/2]         %.3f\n", v_median                       );
-  printf( "4) mad: sort(v-median)[size(v)/2]     %.3f\n", v_mad                          );
+  printf( "1) sum: sum(v)                        %.17e\n", v_sum                         );
+  printf( "2) mean: sum/size(v)                  %.11e\n", v_mean                        );
+  printf( "3) median: sort(v)[size(v)/2]         %.11e\n", v_median                      );
+  printf( "4) mad: sort(v-median)[size(v)/2]     %.11e\n", v_mad                         );
   // implementation-dependent arithmetic types
   printf( "a) Machine epsilon (f):               %e\n",  FLT_EPSILON                     );
   printf( "b) Machine epsilon (ff):              %e\n",  DBL_EPSILON                     );
