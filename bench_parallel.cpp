b354f7ff316b73c44f00cbff91e12cc36e75c34a
Wojciech Lawren <wojciech.lawren@gmail.com>
2025-01-08 19:22:00 +0100

    graph RAII

--- bench_parallel.cpp
+++ bench_parallel.cpp
@@ -7,7 +7,6 @@
 #include <svrng.h>
 #include <oneapi/tbb.h>
 #include <oneapi/tbb/scalable_allocator.h>
-#include <oneapi/tbb/flow_graph.h>
 
 #define SVX (2L << 4L)  // short vector elements 2^n
 typedef double svxdf_t __attribute__ ((vector_size ((SVX * sizeof(double)))));  // short vector arithmetic type
@@ -34,9 +33,12 @@ namespace tpl {
     svrng_engine_t engine;
     svrng_distribution_t distr;
   public:
+    int st = SVRNG_STATUS_OK;  // RNG status
     RNG(const size_t r = MILLE) {
       engine = svrng_new_rand_engine(S);
-      distr  = svrng_new_uniform_distribution_double(1e0, (r / 2e0)); }
+      distr  = svrng_new_uniform_distribution_double(1e0, (r / 2e0));
+      st = svrng_get_status();
+    }
     ~RNG() { svrng_delete_distribution(distr);
              svrng_delete_engine(engine); }
     double unif() const { return svrng_generate_double(engine, distr); }  // proxy with private
@@ -88,6 +90,7 @@ namespace tpl {
   class vec {
     void* ptr;  // vector pointer
   public:
+    int st = TBBMALLOC_OK;  // vec status
     size_t size;  // vector size
     T* begin;   // vector pointer begin
     T* end;     // vector pointer end
@@ -96,6 +99,7 @@ namespace tpl {
     vec(const size_t r = MILLE) {
       size = r;
       ptr = scalable_calloc(size, sizeof(T));
+      if(not ptr) { st = TBBMALLOC_NO_EFFECT; }
       begin = (T*)ptr;
       end = (begin + size);
       sum = mean = median = mad = 0e0;
@@ -125,14 +129,14 @@ namespace tpl {
 
 // trial namespace
 namespace trial {
-  enum FUSION { ON, OFF };  // loop fusion constants
-  thread_local int fusion = FUSION::OFF;  // loop fusion constant
+  const int STATUS_OK = (SVRNG_STATUS_OK | TBBMALLOC_OK);  // constructors ok
+  enum class FUSION { ON = 1, OFF };  // loop fusion constants
+  thread_local FUSION fusion = FUSION::OFF;  // loop fusion constant
   thread_local size_t size = MILLE;  // trial::size
-  thread_local int st = SVRNG_STATUS_OK;  // RNG status
-  int fn(const size_t xch);
+  size_t fn(const size_t xch);
 }  // end trial
 
-int trial::fn(const size_t xch) {
+size_t trial::fn(const size_t xch) {
   trial::size *= xch;
   // diagnostic
   tpl::bench bench;
@@ -141,53 +145,53 @@ int trial::fn(const size_t xch) {
   using svrngx_t = decltype(rng)::svrngx_t;
   // container vector, pointers, reducers
   tpl::vec<double> vec(trial::size);
-  // svrng pointers
-  svrngx_t* rd_begin = (svrngx_t*)(&(*vec.begin));
-  const svrngx_t* rd_end = vec.sv_mod<svrngx_t>();  // SVRNG modulus
-  double* vec_rd = vec.v_mod<svrngx_t>();  // vector modulus
-  // short vector pointers
-  const svxdf_t* sv_begin = (svxdf_t*)(&(*vec.begin));
-  const svxdf_t* sv_end = vec.sv_mod<svxdf_t>();  // short vector modulus
-  const double* vec_sv = vec.v_mod<svxdf_t>();  // vector modulus
-  // reducers
-  svxdf_t sv_sum = { 0e0 };  // short vector reducer
-  const double* svs_0 = (double*)(&sv_sum);  // short vector reducer pointers
-  const double* svs_f = (svs_0 + (sizeof(svxdf_t) / sizeof(double)));
   // double for each
-  switch (fusion) {
-    case FUSION::ON:
-      bench.push(tick_count::now());  // 1&2)
-      for(double* k = vec.begin; k < vec.end; ++k) {
-        *k = rng.unif();
-        vec.sum += *k;
-      }
-      bench.push(tick_count::now());
+  switch ( rng.st = svrng_get_status(); ( rng.st | vec.st ) ) {
+    case STATUS_OK:
+      // svrng pointers
+      svrngx_t* rd_begin = (svrngx_t*)(&(*vec.begin));
+      const svrngx_t* rd_end = vec.sv_mod<svrngx_t>();  // SVRNG modulus
+      double* vec_rd = vec.v_mod<svrngx_t>();  // vector modulus
+      // short vector pointers
+      const svxdf_t* sv_begin = (svxdf_t*)(&(*vec.begin));
+      const svxdf_t* sv_end = vec.sv_mod<svxdf_t>();  // short vector modulus
+      const double* vec_sv = vec.v_mod<svxdf_t>();  // vector modulus
+      // reducers
+      svxdf_t sv_sum = { 0e0 };  // short vector reducer
+      const double* svs_0 = (double*)(&sv_sum);  // short vector reducer pointers
+      const double* svs_f = (svs_0 + (sizeof(svxdf_t) / sizeof(double)));
+      // switch FUSION
+      switch (fusion) {
+        case FUSION::ON:
+          bench.push(tick_count::now());  // 1&2)
+          for(double* k = vec.begin; k < vec.end; ++k) {
+            *k = rng.unif();
+            vec.sum += *k;
+          }
+          bench.push(tick_count::now());
+        break;
+        case FUSION::OFF:
+          bench.push(tick_count::now());  // 1)
+          for(svrngx_t* k = rd_begin; k < rd_end; ++k) { *k = rng.unifsvx(); }
+          for(double* k = vec_rd; k < vec.end; ++k) { *k = rng.unif(); }  // remainder
+          bench.push(tick_count::now());
 
-      st = svrng_get_status();
+          if(( rng.st = svrng_get_status() ) != SVRNG_STATUS_OK) { break; }
+
+          // sum
+          bench.push(tick_count::now());  // 2)
+          for(const svxdf_t* k = sv_begin; k < sv_end; ++k) { sv_sum += *k; }
+          for(const double* k = svs_0; k < svs_f; ++k) { vec.sum += *k; }
+          for(const double* k = vec_sv; k < vec.end; ++k) { vec.sum += *k; }  // remainder
+          bench.push(tick_count::now());
+        break;
+      }  // FUSION
     break;
-    default:
-      bench.push(tick_count::now());  // 1)
-      for(svrngx_t* k = rd_begin; k < rd_end; ++k) { *k = rng.unifsvx(); }
-      for(double* k = vec_rd; k < vec.end; ++k) { *k = rng.unif(); }  // remainder
-      bench.push(tick_count::now());
-
-      if(( st = svrng_get_status() ) != SVRNG_STATUS_OK) { break; }
-
-      // sum
-      bench.push(tick_count::now());  // 2)
-      for(const svxdf_t* k = sv_begin; k < sv_end; ++k) { sv_sum += *k; }
-      for(const double* k = svs_0; k < svs_f; ++k) { vec.sum += *k; }
-      for(const double* k = vec_sv; k < vec.end; ++k) { vec.sum += *k; }  // remainder
-      bench.push(tick_count::now());
-  }
-
+  }  // STATUS
   // mean, median, mad
   // computing or jump according with RNG status
-  switch (st) {
-    case (not SVRNG_STATUS_OK):
-      printf("RNG FAILED: status error %i\n", st);
-    break;
-    default:
+  switch ( rng.st = svrng_get_status(); ( rng.st | vec.st ) ) {
+    case STATUS_OK:
       vec.mean = tpl::mean(vec.sum, vec.size);
 
       bench.push(tick_count::now());  // 3)
@@ -208,12 +212,12 @@ int trial::fn(const size_t xch) {
 
       // print stats
       printf( "%zu) trial size:                        %zu double-precision\n", xch, vec.size);
-    if(fusion == FUSION::ON) {
+    switch (fusion) { case FUSION::ON:
       printf( "1&2) for generate [fused reduce]      %.6fs\n", bench.f1()                    );
-    }else{
+    break; case FUSION::OFF:
       printf( "1) for generate                       %.6fs\n", bench.f1()                    );
       printf( "2) for reduce                         %.6fs\n", bench.f1()                    );
-    }
+    break; }  // FUSION
       printf( "3) parallel_sort                      %.6fs\n", bench.f1()                    );
       printf( "4) parallel_for_each                  %.6fs\n", bench.f1()                    );
       printf( "5) parallel_sort                      %.6fs\n", bench.f1()                    );
@@ -227,18 +231,35 @@ int trial::fn(const size_t xch) {
       printf( "c) Machine epsilon (fff):             %Le\n", LDBL_EPSILON                    );
       printf( "d) Machine rounds style:              %i\n",  FLT_ROUNDS                      );
       printf( "e) x87 FPU exception flags:           %i\n",  fetestexcept(FE_ALL_EXCEPT)     );
-  }
-  return st;
-}
+    break;
+    default:
+      printf( "APP FAILED: status rng: %i | tbb: %i\n", rng.st, vec.st );
+    return 0L;
+  }  // STATUS
+  return trial::size;
+}  // trial::fn
 
 int main() {
+  size_t sum = 0L;
   // graph parallelism
   flow::graph graph;
-  flow::function_node<size_t, int, flow::queueing> fn(graph, info::default_concurrency(),
-                                                      [](const size_t v) { return trial::fn(v); });
-
+  // function_node
+  flow::function_node<size_t, size_t, flow::queueing> fn(graph, flow::unlimited,  // flow::serial
+    [&](const size_t &v) { return trial::fn(v); });
+  // function_node
+  flow::function_node<size_t, size_t, flow::queueing> summer(graph, flow::serial,
+    [&](const size_t &v) { return sum += v; });
+  // edge
+  flow::make_edge(fn, summer);
+  // execute incoming message
   for(size_t i = 1L; i < 2L; ++i) { fn.try_put(i); }
-  graph.wait_for_all();
+  // block & catch & throw
+  try { graph.wait_for_all(); }
+  catch (...) { graph.cancel(); throw; }
+  // print info
+  printf( "%s\n", "===" );
+  printf( "a) graph exception thrown:            %i\n", graph.exception_thrown()   );
+  printf( "b) trial size sum:                    %zu\n", sum                       );
 
   return 0;
 }
