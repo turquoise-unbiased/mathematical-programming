4af2e60d28bca5ac8a7f225d2a79d3bfa5c49141
Wojciech Lawren <wojciech.lawren@gmail.com>
2025-09-25 09:12:53 +0200

    {opt|cls}_uninte.R

---
+++ opt_uninte.R
@@ -0,0 +1,32 @@
+## opt_uninte.R [4.4.1] 2024, Wojciech Lawren, All rights reserved.
+## Optimizing [p.r.] moment / interval | collision of pseudo-random integers of uniform distribution
+library("parallel")
+.cc <- parallel::detectCores()  # number of CPU cores >=1 nodes involved in simulation
+
+source("cls_uninte.R")
+assign("m", new("Sim"), envir = ( e.sim <- new.env() ))  # str class Sim
+
+parameters <- function() {
+  nodes <- min((3L * m@k), .cc)
+  ikeys <- seq(from = 37L, by = 337L, length = nodes)
+  cl <- parallel::makeForkCluster(nodes)                       # cluster of processes
+  on.exit(parallel::stopCluster(cl))
+  parallel::clusterApply(cl, ikeys, function(r) set.seed(r))  # cycle of pseudo-random numbers
+  parallel::parApply(cl, eval(m@A), c(2L, 3L), function(r) v.switch(r))      # parallel apply
+}
+
+simulate <- function(P) {
+  nodes <- min((m@t ^ 3L * m@k), .cc)
+  ikeys <- seq(from = 59L, by = 157L, length = nodes)
+  cl <- parallel::makeForkCluster(nodes)                 # cluster of processes
+  on.exit(parallel::stopCluster(cl))
+  parallel::clusterApply(cl, ikeys, function(r) set.seed(r))  # cycle of pseudo-random numbers
+  parallel::parApply(cl, P, c(1L, 3L), function(r) orbit(r))              # parallel apply
+}
+
+attach(e.sim, pos = 2L)
+S <- aperm(simulate(parameters()), perm = c(2L, 1L, 3L))  # score array
+
+colnames(S) <- m@col
+if (m@show) { for (i in seq(m@k)) { print(summary(S[,,i])) ; print(cor(S[,,i])) }}
+if (m@save) { write.csv(S, m@csv) }
